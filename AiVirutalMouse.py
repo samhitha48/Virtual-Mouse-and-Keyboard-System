935ee4835e26ea9f2f6dc086dc70354cda68972a
Sannidhi Samhitha <73403349+samhitha48@users.noreply.github.com>
2024-02-23 03:02:05 -0500

    Update AiVirutalMouse.py

--- AiVirutalMouse.py
+++ AiVirutalMouse.py
@@ -19,8 +19,11 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 detector = htm.handDetector(maxHands=1)
 wScr, hScr = autopy.screen.size()
-#print(wScr, hScr)
 
+
+mouse_position = (0, 0)
+mouse_down = False
+mouse_button = None 
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
@@ -30,10 +33,10 @@ while True:
         x1, y1 = lmList[8][1:]
         x2, y2 = lmList[12][1:]
 
-        #print(x1, y1, x2, y2)
+        
 
+        
         fingers = detector.fingersUp()
-        #print(fingers)
         cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
                       (255, 0, 255), 2)
         # 4. Only index Finger :Moving Mode
@@ -45,12 +48,11 @@ while True:
             # 6. Smoothen Values
             clocX = plocX +(x3 -plocX) /smoothening
             clocY = plocY + (y3 - plocY) / smoothening
-
-            # 7. Move Mouse
+            #7. Mouse Move 
             autopy.mouse.move(wScr-clocX, clocY)
-            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv.2circle(img (x1, y1), 15, (255, 0, 255), cv2.FILLED)
             plocX, plocY = clocX, clocY
-
+            
         # 8. Both Index and Middle Finger are up : Clicking Mode
         if fingers[1] == 1 and fingers[2] == 1:
             length, img, lineInfo = detector.findDistance(8, 12, img)
@@ -59,13 +61,15 @@ while True:
                 cv2.circle(img, (lineInfo[4], lineInfo[5]),
                            15, (0, 255, 0), cv2.FILLED)
                 autopy.mouse.click()
-        # 9. Find Distance between Fingers
+        if fingers[1] == 1 and fingers[2] == 1:
+            length, img, lineInfo = detector.findDistance(8, 12, img)
+            print(length)
+            if (length> 39):
+                pyautogui.click(button='right')
+        if fingers[3]==1  
+        pyautogui.scroll(-2)
+        if fingers[2]==1  
+        pyautogui.scroll(2)
 
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, str(int(fps)), (40, 50), cv2.FONT_HERSHEY_PLAIN, 3,
-                (255, 0, 0), 3)
-
-    cv2.imshow("Img", img)
-    cv2.waitKey(1)
+    cv2.imshow("Virtual Mouse", img)
+    cv2.waitKey(100)
